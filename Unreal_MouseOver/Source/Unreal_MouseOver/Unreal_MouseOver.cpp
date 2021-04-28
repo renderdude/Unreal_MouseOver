fac9b4c31fa28b69f86160f62c5c1c48d15ac83c
Mark Bolstad <the.render.dude@gmail.com>
2021-04-27 22:39:09 -0600

    Add the Unreal Project files.

---
+++ Unreal_MouseOver/Source/Unreal_MouseOver/Unreal_MouseOver.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Unreal_MouseOver.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Unreal_MouseOver, "Unreal_MouseOver" );
